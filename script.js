43c424dd4107a6235202c83fb800edc71a29e0b6
nora <noraashqar244@gmail.com>
2025-08-15 01:15:21 +0300

    adding ckeckbox and delete button for each task

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function addTask(){
     if(newTask.value.trim()!=""){
          const task = document.createElement("li");
           task.textContent=newTask.value;
-           tasksList.append(task); newTask.value="";
+ 
+           const checkBox =document.createElement("input");
+           checkBox.type="checkbox";
+           checkBox.className="done";
+           task.append(checkBox);
+
+           const deleteButton =document.createElement("button");
+           deleteButton.textContent= 'delete';
+           deleteButton.className= 'deleteBtn';
+           task.append(deleteButton);
+           
+           tasksList.append(task); 
+           newTask.value="";
          } 
         } 
 addButton.addEventListener('click',addTask)
