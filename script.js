7276c73bd0a61eaca853ae7abf1d9edc1e5d42df
nora <noraashqar244@gmail.com>
2025-08-27 23:36:58 +0300

    add local storage

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const newTask =document.getElementById('newTask');
 const tasksList= document.getElementById('tasks');
 const addButton= document.getElementById('addBtn');
 
-function addTask(){ 
-    if(newTask.value.trim()!=""){
-         const task = document.createElement("li");
-          task.textContent=newTask.value;
+document.addEventListener("DOMContentLoaded",loadTasks);
+function addTask(taskStrg){ 
+    const taskT= taskStrg||newTask.value.trim();
+    if(taskT!=""){
+         const task= document.createElement("li");
+          task.textContent=taskT;
  
            const checkBox =document.createElement("input");
            checkBox.type="checkbox";
@@ -19,10 +21,13 @@ function addTask(){
            task.append(deleteButton);
 
            tasksList.append(task); 
+           
+           if (!taskStrg) {
+            saveTask(taskT);}
            newTask.value="";
     } 
 } 
-addButton.addEventListener('click',addTask)
+addButton.addEventListener('click',()=>addTask())
 newTask.addEventListener('keypress',function(e){
      if(e.key=='Enter'){ 
         addTask() 
@@ -37,6 +42,32 @@ tasksList.addEventListener('click',function(e){
     }
     if(e.target.classList.contains('deleteBtn'))
     {
+        const text =e.target.parentElement.firstChild.textContent;
+        removeTask(text);
         e.target.parentElement.remove();
     }
-})    
+}) 
+
+function saveTask(task) {
+    const stored = localStorage.getItem("tasks");
+    const parsed = JSON.parse(stored) || [];
+    parsed.push(task);
+    const listToStore = JSON.stringify(parsed);
+    localStorage.setItem("tasks", listToStore);
+}
+
+function removeTask(task){
+    const stored=localStorage.getItem("tasks");
+    const parsed =JSON.parse(stored)||[];
+    const filtered =parsed.filter(t=>t!==task);
+    const listToStore= JSON.stringify(filtered);
+    localStorage.setItem("tasks",listToStore);
+
+}
+function loadTasks() {
+    const stored = localStorage.getItem("tasks");
+    const parsed = JSON.parse(stored) || [];
+    parsed.forEach(task => {
+        addTask(task);
+    });
+}
